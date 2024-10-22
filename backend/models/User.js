9d4e90fa02ab4802938d6fa2448b8e9dc605dd4d
Alankc <122060247+sennsy12@users.noreply.github.com>
2024-10-22 22:37:47 +0200

    t

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
-const User = sequelize.define('User', {
+const User = sequelize.define('user', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
